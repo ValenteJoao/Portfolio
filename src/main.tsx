7fcf755494949071593d05e74d592a24d285103a
Valente <108316067+ValenteJoao@users.noreply.github.com>
2025-05-02 17:34:54 -0300

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>,
 )
